9a891c8f6291a48b5c342d9189aeb77d4f3e76de
MainLah <rendrakusumab@gmail.com>
2025-06-12 19:17:18 +0700

    fix: kelewat lagi link api

--- src/utils/handleLogout.ts
+++ src/utils/handleLogout.ts
@@ -1,6 +1,6 @@
 const handleLogout = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/logout", {
+    const res = await fetch("https://cungur-v2.vercel.app/api/auth/logout", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
